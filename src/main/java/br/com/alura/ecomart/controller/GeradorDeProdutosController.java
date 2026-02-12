8bb988b2fbe9b3046feab11d9c61e39e69970d3e
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-12 10:02:30 -0300

    Implemento código do Chat Client API no controller

--- src/main/java/br/com/alura/ecomart/controller/GeradorDeProdutosController.java
+++ src/main/java/br/com/alura/ecomart/controller/GeradorDeProdutosController.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecomart.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("gerador")
 public class GeradorDeProdutosController {
 
+    // Atributo
+    private final ChatClient chatClient;
+
+    // Construtor
+    public GeradorDeProdutosController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    // Metodos
     @GetMapping
     public String gerarProdutos() {
-        return "";
+        var pergunta = "Gere 5 produtos ecologicos";
+        return this.chatClient.prompt()
+                .user(pergunta)
+                .call()
+                .content();
     }
 
 }
