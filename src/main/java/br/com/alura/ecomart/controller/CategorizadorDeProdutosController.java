9090e447dbe7798ff614a1829d6d6ee07d1c7fe0
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-02-13 02:19:37 -0300

    Add metodo para contar tokens

--- src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
+++ src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
@@ -1,5 +1,8 @@
 package br.com.alura.ecomart.controller;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.EncodingType;
+import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.DefaultChatOptionsBuilder;
@@ -45,6 +48,11 @@ public class CategorizadorDeProdutosController {
                 Resposta: Esportes
                 """;
 
+        var tokens = contarTokens(system, produto);
+        System.out.println("QTD de tokens: " + tokens);
+
+        // Fazer lógica de escolha de modelo baseada no número de tokens
+
         return this.chatClient.prompt()
                 .system(system)
                 .user(produto)
@@ -54,6 +62,12 @@ public class CategorizadorDeProdutosController {
                         .build())
                 .call()
                 .content();
+    }
+
+    private int contarTokens(String system, String user) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_4O);
+        return enc.countTokens(system + user);
     }
 
 }
